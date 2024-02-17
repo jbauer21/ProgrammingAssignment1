c7930bf8b00822c3ac34e1494fd30390e4576388
Julian Bauer <julianbauer@Julians-MacBook-Pro.local>
2024-02-16 19:27:03 -0600

    fixed binConstServer

--- src/BinConstServer.java
+++ src/BinConstServer.java
@@ -2,5 +2,7 @@ public interface BinConstServer {
     public static final String defaultEncoding = "UTF-16BE";
     public static final byte maxTML = 8; //Maximum message length
     public static final int maxResult = Integer.MAX_VALUE;
-    public static final int maxResult = Integer.MAX_VALUE;
+    public static final byte errorCode = 127;
+    public static final byte passCode = 0;
+
 }
